cc1cd8b3b3cf2b0b5932cca29ba3da9e8c100df0
durgesh123-iitr <durgesh123.iitr@gmail.com>
2018-11-27 00:06:59 +0530

    about page feedback and flotting done

---
+++ altway/js/dmain.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items:1,
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
+})
+
