d345ef2caca8fe44c2db990693cc358141651032
Primus <you@example.com>
2018-12-01 00:58:57 +0530

    owl carousel for mentor and mentee

--- altway/js/dmain.js
+++ altway/js/dmain.js
@@ -4,6 +4,5 @@ $('.owl-carousel').owlCarousel({
     animateOut: 'slideOutDown',
     animateIn: 'flipInX',
     items:1,
-    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
-})
-
+    navText : ['<i class="fa fa-arrow-left" aria-hidden="true"></i>','<i class="fa fa-arrow-right" aria-hidden="true"></i>']
+});
