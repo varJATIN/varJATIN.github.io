461e04e3d1a4cd2da120120c70c6156f028baea2
durgesh123-iitr <durgesh123.iitr@gmail.com>
2018-12-02 06:40:11 +0530

    cutoo cutoo cutoo

--- altway/js/dmain.js
+++ altway/js/dmain.js
@@ -6,3 +6,10 @@ $('.owl-carousel').owlCarousel({
     items:1,
     navText : ['<i class="fa fa-arrow-left" aria-hidden="true"></i>','<i class="fa fa-arrow-right" aria-hidden="true"></i>']
 });
+
+$('.active-brand-carusel').owlCarousel({
+    loop:true,
+    margin:10,
+    items:3,
+    navText : ['<i class="fa fa-arrow-left" aria-hidden="true"></i>','<i class="fa fa-arrow-right" aria-hidden="true"></i>']
+});
